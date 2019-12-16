829daee7f1ff5d39b1f25fb955caf1e496bf08df
Daniel RT <daniriian@gmail.com>
2019-12-16 23:20:49 +0200

    add overlay on mobile view with menu opened

--- src/components/Navbar/MenuButton/MenuButton.component.jsx
+++ src/components/Navbar/MenuButton/MenuButton.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './MenuButton.style.scss';
 
-const MenuButton = () => {
+const MenuButton = props => {
   return (
-    <button className="hamburger">
-      <div className="bars">
-        <div className="button-bar"></div>
-        <div className="button-bar"></div>
-        <div className="button-bar"></div>
+    <button type='button' className='hamburger' onClick={props.onClick}>
+      <div className='bars'>
+        <div className='button-bar'></div>
+        <div className='button-bar'></div>
+        <div className='button-bar'></div>
       </div>
     </button>
   );
--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MenuButton from './MenuButton/MenuButton.component';
 
 import './Navbar.styles.scss';
 
 const Navbar = props => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const inverseMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <nav className="nav">
-      <div className="nav-container">
-        <div className="logo">
-          <a href="/">{props.logo}</a>
+    <nav className='nav' onClick={inverseMenu}>
+      <div className={'overlay' + (menuOpen ? ' add_overlay' : '')}></div>
+      <div className='nav-container'>
+        <div className='logo'>
+          <a href='/'>{props.logo}</a>
         </div>
-        <MenuButton />
-        <div className="menu">
+        <MenuButton onClick={inverseMenu} />
+        <div className={'menu' + (menuOpen ? ' menu_open' : '')}>
           <ul>
-            <li className="menu-item">link</li>
-            <li className="menu-item">link</li>
-            <li className="menu-item">link</li>
-            <li className="menu-item">link</li>
-            <li className="menu-item">link</li>
+            <li className='menu-item'>
+              <a href='www.yahoo.com'>Link</a>
+            </li>
+            <li className='menu-item'>
+              <a href='www.yahoo.com'>Link</a>
+            </li>
+            <li className='menu-item'>
+              <a href='www.yahoo.com'>Link</a>
+            </li>
+            <li className='menu-item'>
+              <a href='www.yahoo.com'>Link</a>
+            </li>
           </ul>
         </div>
       </div>
