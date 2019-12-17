62ecc32b24605000020cefc865f7f4921f97052c
DRT <dryan251@gmail.com>
2019-12-17 08:19:18 +0200

    menu items added programatically as Navbar props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.component';
 
+const menuLinks = [
+  { address: 'https://www.google.ro', name: 'Google' },
+  { address: 'https://www.yahoo.com', name: 'Yahoo' }
+];
+
 function App() {
   return (
     <div className="App">
-      <Navbar logo="Bellesa" />
+      <Navbar logo="Bellesa" menuItems={menuLinks} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import MenuButton from './MenuButton/MenuButton.component';
 
 import './Navbar.styles.scss';
 
-const Navbar = props => {
+const Navbar = ({ logo, menuItems }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const inverseMenu = () => {
@@ -12,31 +12,24 @@ const Navbar = props => {
   };
 
   return (
-    <nav className='nav'>
+    <nav className="nav">
       <div
         className={'overlay' + (menuOpen ? ' add_overlay' : '')}
-        onClick={inverseMenu}
+        onClick={menuOpen ? inverseMenu : null}
       ></div>
 
-      <div className='nav-container'>
-        <div className='logo'>
-          <a href='/'>{props.logo}</a>
+      <div className="nav-container">
+        <div className="logo">
+          <a href="/">{logo}</a>
         </div>
         <MenuButton onClick={inverseMenu} />
         <div className={'menu' + (menuOpen ? ' menu_open' : '')}>
           <ul>
-            <li className='menu-item'>
-              <a href='https://www.yahoo.com'>Link</a>
-            </li>
-            <li className='menu-item'>
-              <a href='www.yahoo.com'>Link</a>
-            </li>
-            <li className='menu-item'>
-              <a href='www.yahoo.com'>Link</a>
-            </li>
-            <li className='menu-item'>
-              <a href='www.yahoo.com'>Link</a>
-            </li>
+            {menuItems.map(item => (
+              <li className="menu-item">
+                <a href={item.address}>{item.name}</a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
